29a6835c612e5fff1ece32abe70eaa3cd22b6904
abnersolivera <abnersanto2014@gmail.com>
2024-04-22 20:08:45 -0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-primeira-aula';
+  nameAdded = 'Curso de Angular';
+  ageAdded = 0;
+  idRemoved = 0;
+
+  values = [
+    {
+      name: 'João',
+      age: 20,
+      id: 1
+    },
+    {
+      name: 'Abner',
+      age: 27,
+      id: 2
+    },
+    {
+      name: 'Stéffany',
+      age: 24,
+      id: 3
+    }   
+  ];
+
+  premiumCustomer = false;
+
+  added(){
+    let idAdded = this.values.length + 1;
+    this.values.push({name: this.nameAdded, age: this.ageAdded, id: idAdded});
+  }
+
+  remove(){
+    this.values.pop();
+  }
 }
